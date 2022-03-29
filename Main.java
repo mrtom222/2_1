3bbf3f40c0353b978ae668bb2d9c3dca70c935fa
mrtom222 <38867467+mrtom222@users.noreply.github.com>
2022-03-29 10:31:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int w = 5;
+    for(int i = 0;i<w;i++){
+      for(int j = 0;j<(i+1);j++){
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+}
